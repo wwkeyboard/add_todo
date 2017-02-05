c26c9c1d16c4760399fda855463bf755e319fcd9
Aaron Lee <wwkeyboard@gmail.com>
2017-02-05 13:09:38 -0600

    clean up boilerplate code

--- cmd/root.go
+++ cmd/root.go
@@ -1,17 +1,3 @@
-// Copyright © 2017 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package cmd
 
 import (
@@ -19,7 +5,6 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 	"github.com/wwkeyboard/goorg"
 )
 
@@ -32,9 +17,7 @@ var RootCmd = &cobra.Command{
 	Long: `Parses an org-mode file and adjusts is
 This is meant to have an interaction similar to taskwarrior.
 `,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Run: run,
 }
 
 // Execute adds all child commands to the root command sets flags appropriately.
@@ -49,29 +32,20 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports Persistent Flags, which, if defined here,
-	// will be global for your application.
-
 	RootCmd.PersistentFlags().StringVar(&orgFile, "file", "", "org file to operate on")
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
+
 	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
-	_, err := goorg.FromFile(orgFile)
+}
+
+func run(cmd *cobra.Command, args []string) {
+	org, err := goorg.FromFile(orgFile)
 	if err != nil {
 		fmt.Println("Couldn't read org-mode file")
 	}
 
-	viper.SetConfigName(".add_todo") // name of config file (without extension)
-	viper.AddConfigPath("$HOME")     // adding home directory as first search path
-	viper.AutomaticEnv()             // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
+	fmt.Printf("Read %v toplevel headlines\n", len(org.Headlines))
 }
