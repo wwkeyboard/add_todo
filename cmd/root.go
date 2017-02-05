2bcbb77afe75ae894c237d00290eff0fc2ab75f9
Aaron Lee <wwkeyboard@gmail.com>
2017-02-05 09:57:38 -0600

    Better describe this project

--- cmd/root.go
+++ cmd/root.go
@@ -28,13 +28,10 @@ var orgFile string
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "add_todo",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Adds todo items to an org-mode file",
+	Long: `Parses an org-mode file and adjusts is
+This is meant to have an interaction similar to taskwarrior.
+`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
